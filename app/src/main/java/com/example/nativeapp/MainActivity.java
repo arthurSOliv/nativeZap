eb0cc77185d64b40693b5c803c0703b85ed7d96a
Arthur de Souza Oliveira <arthur.oliveira@bravolog.com.br>
2021-02-09 22:49:36 -0300

    First commit

---
+++ app/src/main/java/com/example/nativeapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.nativeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //referenciaFirebase.child("pontos").setValue(100);
+    }
+}
